fa51101c4af1dc307956d13c4d961a3b6ce1b8d0
Preetika Lakshmanan <preetika19@gmail.com>
2022-12-29 17:32:04 +0400

    final

---
+++ logistic_regression.py
@@ -0,0 +1,123 @@
+import os
+import re
+import numpy as np
+
+def readFile(path):
+    files = os.listdir(path)
+    vocabulary = []
+    d = {}
+    for file in files:
+        f = open(path+"/"+file,encoding = "ISO-8859-1")
+        text = f.read()
+        text = re.sub('[^a-zA-Z]', ' ', text)
+        words_list = text.strip().split()
+        d[file] = words_list
+        vocabulary.extend(words_list)
+    return vocabulary, d
+
+def extractKeys(train_spam_list,train_ham_list):
+    return list(set(train_spam_list)|set(train_ham_list))
+
+def getClassLabels(num_spam_file, num_ham_file):
+    assigned_class = []
+    for i in range(num_spam_file):
+        assigned_class.append(0)
+    for j in range(num_ham_file):
+        assigned_class.append(1)
+    return assigned_class
+
+def featureList(words, dic1):
+    w = list(words)
+    result = []
+    for f in dic1:
+        row = [0] * (len(w))
+        for word in w:
+            if word in dic1[f]:
+                row[w.index(word)] = 1
+        row.insert(0,1)
+        result.append(row)
+    return result
+
+def mergeData(dataset1, dataset2):
+    dic3 = dataset1.copy()
+    dic3.update(dataset2)
+    return dic3
+
+def sigmoid(z):
+    return 1.0/(1+np.exp(-z))
+
+def trainLR(data_matrix,class_labels,Lambda):
+    data_set = np.mat(data_matrix)
+    label_set = np.mat(class_labels).transpose()
+    m,n = np.shape(data_set)
+    alpha = 0.1
+    iterations = 100
+    weights = np.zeros((n,1))
+    for k in range(iterations):
+        print("Iteration number -", k)
+        h = sigmoid(data_set*weights)
+        error = (label_set - h)
+        weights = weights + alpha * data_set.transpose() * error - alpha * Lambda * weights
+    return weights
+
+
+def classify(weight,data,num_spam,num_ham):
+    matrix = np.mat(data)
+    wx = matrix * weight
+    correct = 0
+    spam = 0
+    ham = 0
+    total = num_spam + num_ham
+
+    for i in range(num_spam):
+        if wx[i][0] < 0.0:
+            correct += 1
+            spam += 1
+    for j in range(num_spam+1,total):
+        if wx[j][0] > 0.0:
+            correct += 1
+            ham +=1
+    
+    print("Number of Ham emails correctly classifed: " , ham)
+    print("Number of Ham emails incorrectly classifed: " , len(test_ham_dict) - ham)
+    print("Ham Accuracy: ", (ham / len(test_ham_dict)) * 100)
+    print("Number of Spam emails correctly classifed: " , spam)
+    print("Number of Spam emails incorrectly classifed: " , len(test_spam_dict) - spam)
+    print("Spam Accuracy: ", (spam / len(test_spam_dict)) * 100)
+    print("Total Accuracy: ", 1.0 * correct/total *100)
+    return wx
+
+
+if __name__ == "__main__":
+    train_spam_path = r'train/spam'
+    train_ham_path = r'train/ham'
+    test_spam_path = r'test/spam'
+    test_ham_path = r'test/ham'
+    
+    train_spam_list, train_spam_dict = readFile(train_spam_path)
+    train_ham_list, train_ham_dict = readFile(train_ham_path)
+        
+    test_spam_list, test_spam_dict = readFile(test_spam_path)
+    test_ham_list, test_ham_dict = readFile(test_ham_path)
+    
+    words = extractKeys(train_spam_list,train_ham_list)
+    train = mergeData(train_spam_dict,train_ham_dict)
+    test = mergeData(test_spam_dict,test_ham_dict)
+
+    num_train_spam = len(train_spam_dict)
+    num_test_spam = len(test_spam_dict)
+    num_train_ham= len(train_ham_dict)
+    num_test_ham = len(test_ham_dict)
+
+    train_labels = getClassLabels(num_train_spam,num_train_ham)
+
+    train_data_list = featureList(words, train)
+    test_data_list = featureList(words, test)
+    
+    Lambda = float(input("Enter Lambda value:"))
+    
+    print("Number of test Ham emails: " , len(test_ham_dict))
+    print("Number of test Spam emails: " , len(test_spam_dict))
+    weight = trainLR(train_data_list, train_labels, Lambda)
+    test = classify(weight, test_data_list, num_test_spam, num_test_ham)
+    
---
+++ naive_bayes_stopword_removal.py
@@ -0,0 +1,121 @@
+import os
+import re
+from math import log
+from collections import Counter
+from nltk.corpus import stopwords
+
+def readFile(path):
+    files = os.listdir(path)
+    vocabulary = []
+    d = {}
+    for file in files:
+        f = open(path+"/"+file,encoding = "ISO-8859-1")
+        text = f.read()
+        text = re.sub('[^a-zA-Z]', ' ', text)
+        words_list = text.strip().split()
+        d[file] = words_list
+        vocabulary.extend(words_list)
+    return vocabulary, d
+
+def readStopWords(path, stop_words):
+    files = os.listdir(path)
+    vocabulary = []
+    d = {}
+    for file in files:
+        f = open(path+"/"+file,encoding = "ISO-8859-1")
+        text = f.read()
+        text = re.sub('[^a-zA-Z]', ' ', text)
+        words_list = text.strip().split()
+        words_new = []
+        for w in words_list:
+            if w not in stop_words:
+                words_new.append(w)
+        d[file] = words_new
+        vocabulary.extend(words_new)
+    return vocabulary, d
+
+def extractKeys(train_spam_list,train_ham_list):
+    return list(set(train_spam_list)|set(train_ham_list))
+
+def train_NB(count_spam, count_ham, spam_fre_dic, ham_fre_dic, spam_list, ham_list, key_list):
+    prior_spam = 1.0 * count_spam / (count_spam + count_ham)
+    prior_ham = 1.0 * count_ham / (count_spam + count_ham)
+    total_spam = len(spam_list)
+    total_ham = len(ham_list)
+    total_type = len(key_list)
+    add_one = 1
+    cond_pro_spam = {}
+    cond_pro_ham = {}
+
+    for w in key_list:
+        occurrence = 0
+        if w in spam_fre_dic:
+            occurrence = spam_fre_dic[w]
+        cond_prob1 = 1.0 * (occurrence + add_one) / (total_spam + total_type)
+        cond_pro_spam[w] = cond_prob1
+
+    for w in key_list:
+        occurrence = 0
+        if w in ham_fre_dic:
+            occurrence = ham_fre_dic[w]
+        cond_prob2 = 1.0 * (occurrence + add_one) / (total_ham + total_type)
+        cond_pro_ham[w] = cond_prob2
+
+    return prior_spam, prior_ham, cond_pro_spam, cond_pro_ham
+
+
+def apply_NB(prior_spam, prior_ham, cond_pro_spam, cond_pro_ham, spam_dic, ham_dic, key_list):
+    set_dic = [spam_dic, ham_dic]
+    total = len(spam_dic) + len(ham_dic)
+    correct = 0
+    spam = 0
+    ham = 0
+    for i in range(len(set_dic)):
+        for f_name in set_dic[i]:
+            score1 = log(prior_spam)
+            score2 = log(prior_ham)
+            for word in set_dic[i][f_name]:
+                if word in key_list:
+                    score1 += log(cond_pro_spam[word])
+                    score2 += log(cond_pro_ham[word])
+            if score1 >= score2 and i == 0:
+                correct +=1
+                spam +=1
+            elif score1 <= score2 and i == 1:
+                correct +=1
+                ham +=1
+    print("Number of Ham emails correctly classifed: " , ham)
+    print("Number of Ham emails incorrectly classifed: " , len(test_ham_dict) - ham)
+    print("Ham Accuracy: ", (ham / len(test_ham_dict)) * 100)
+    print("Number of Spam emails correctly classifed: " , spam)
+    print("Number of Spam emails incorrectly classifed: " , len(test_spam_dict) - spam)
+    print("Spam Accuracy: ", (spam / len(test_spam_dict)) * 100)
+    return 1.0 * correct/total
+
+
+if __name__ == "__main__":
+    train_spam_path = r'train/spam'
+    train_ham_path = r'train/ham'
+    test_spam_path = r'test/spam'
+    test_ham_path = r'test/ham'
+    stop_words = set(stopwords.words('english'))
+    
+    train_spam_list, train_spam_dict = readStopWords(train_spam_path,stop_words)
+    train_ham_list, train_ham_dict = readStopWords(train_ham_path,stop_words)
+        
+    test_spam_list, test_spam_dict = readFile(test_spam_path)
+    test_ham_list, test_ham_dict = readFile(test_ham_path)
+
+    num_spam = len(train_spam_dict)
+    num_ham = len(train_ham_dict)
+
+    spam_freq = Counter(train_spam_list)
+    ham_freq = Counter(train_ham_list)
+    key_list = extractKeys(spam_freq, ham_freq)
+    
+    print("Number of test Ham emails: " , len(test_ham_dict))
+    print("Number of test Spam emails: " , len(test_spam_dict))
+    prior_spam, prior_ham, cond_prob1, cond_prob2 = train_NB(num_spam, num_ham,spam_freq, ham_freq, train_spam_list, train_ham_list,key_list)
+    funct = apply_NB(prior_spam, prior_ham, cond_prob1, cond_prob2, test_spam_dict, test_ham_dict, key_list)
+    print("Total Accuracy: ", funct * 100)
+    
